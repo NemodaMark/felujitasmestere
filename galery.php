1922b4adac2879cd5383521fdbe5196166413da2
Nemoda Márk  Levente <mark.nemoda@gmail.com>
2024-01-29 11:11:01 +0100

    Galery almost completed

---
+++ galery.php
@@ -0,0 +1,43 @@
+<?php
+// Enable error reporting and display errors
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+?>
+
+<?php include_once("Header/header.php"); ?>
+
+<style>
+    .card-img-top {
+        height: 400px; /* Set a fixed height for the images */
+        width: auto; /* Let the width adjust automatically to maintain aspect ratio */
+        object-fit: cover; /* Ensure the image covers the entire space */
+        border-radius: 5px;
+    }
+</style>
+
+
+<div class="row">
+    <h3 class="text-center">Képek a munkáinkról</h3>
+    <?php
+    // Specify the directory where your images are located
+    $imageDirectory = "imgs/workphotos";
+
+    // Get a list of all files in the directory
+    $files = scandir($imageDirectory);
+
+    // Loop through each file
+    foreach ($files as $file) {
+        // Check if the file is a regular file and ends with a known image extension
+        if (is_file($imageDirectory . '/' . $file) && preg_match('/\.(jpg|jpeg|png|gif)$/i', $file)) {
+            // Output the HTML img tag with the file path
+            echo '<div class="col-4">';
+            echo '<div class="card mx-2 my-2">';
+            echo '<img class="card-img-top" src="' . $imageDirectory . '/' . $file . '" alt="' . $file . '" />';
+            echo '</div>';
+            echo '</div>';
+        }
+    }
+    ?>
+</div>
+
+<?php include_once("Header/footer.php"); ?>
--- index.php
+++ index.php
@@ -1,9 +1,29 @@
-<?php include_once("Header/header.php"); ?>
-
-    <h1>SADASD AS </h1>
-
-
 <?php
+// Enable error reporting and display errors
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 ?>
+
+<?php include_once("Header/header.php"); ?>
+
+<style>
+    .col-3 img {
+        max-width: 100%; /* Set the maximum width of the image to 100% of its container */
+        height: auto; /* Let the height adjust automatically to maintain aspect ratio */
+        border-radius: 5px;
+    }
+</style>
+
+<div class="row">
+    <div class="col-9">
+        <h4>Üdvözlünk a HomlokzatHőse weboldalán! Cégünk évek óta az otthonok védelmének és felújításának szenteli magát. 
+            Szakértelmünk és elkötelezettségünk révén olyan megoldásokat kínálunk ügyfeleinknek, amelyek garantálják otthonuk hosszú távú biztonságát és esztétikai megjelenését.
+            Az épületek homlokzata az otthonok arca, és mi, mint a homlokzatok hősei, minden erőnkkel azon dolgozunk, hogy ez az arculat mindig ragyogó legyen. 
+            Legyen szó festésről, szigetelésről vagy bármilyen más homlokzati megoldásról, mi itt vagyunk, hogy segítsünk álmaid otthonát valóra váltani.</h4>
+    </div>
+    <div class="col-3">
+        <img src="imgs/workphotos/421103696_1085069212915028_5634653405770559295_n.jpg" alt="">
+    </div>
+</div>
+
+<?php include_once("Header/footer.php"); ?>
